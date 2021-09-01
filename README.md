7a273efd346ee79699e54ccb8317c996de38bfc2
mariacamila0712 <mariacmaza7@gmail.com>
2021-09-01 13:35:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-yaztg9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-yaztg9)
---
+++ interfaces/Result.interfaces.ts
@@ -0,0 +1,14 @@
+export interface IResults {
+  gender: string;
+  name: IName;
+  location: ILocation;
+  email: string;
+  login: ILogin;
+  dob: IDob;
+  registered: IRegistered;
+  phone: number;
+  cell: number;
+  id: IId;
+  picture: IPicture;
+  nat: string;
+}
